8b7fe6f5cd9ded9d6b293bdbbf865078bd1158d6
Chris Pressey <cpressey@catseye.tc>
2016-03-31 16:04:02 +0100

    Remove deprecated `test` argument when running `falderal`.

--- test.sh
+++ test.sh
@@ -14,22 +14,22 @@ make clean all >ERRORS 2>&1 || error $?
 
 echo "Testing default build..."
 TESTS="../tests/Assembly.markdown ../tests/Term.markdown"
-falderal -b test $TESTS >ERRORS 2>&1 || error $?
+falderal -b $TESTS >ERRORS 2>&1 || error $?
 
 make clean all CFLAGS=-DDIRECT_THREADING >ERRORS 2>&1 || error $?
 
 echo "Testing direct threading build..."
-falderal -b test $TESTS >ERRORS 2>&1 || error $?
+falderal -b $TESTS >ERRORS 2>&1 || error $?
 
 make clean tool >ERRORS 2>&1 || error $?
 
 echo "Testing 'tool' build..."
-falderal -b test $TESTS >ERRORS 2>&1 || error $?
+falderal -b $TESTS >ERRORS 2>&1 || error $?
 
 make clean static >ERRORS 2>&1 || error $?
 
 echo "Testing 'static' build..."
-falderal -b test $TESTS >ERRORS 2>&1 || error $?
+falderal -b $TESTS >ERRORS 2>&1 || error $?
 
 echo "Building 'debug' version..."
 make clean debug >ERRORS 2>&1 || error $?
